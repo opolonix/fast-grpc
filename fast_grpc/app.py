80c78e32bce1f983cd883c51136365fd835793fb
OPOLO <misha90638@gmail.com>
2024-06-12 21:03:16 +0300

    Update app.py

--- fast_grpc/app.py
+++ fast_grpc/app.py
@@ -7,6 +7,7 @@ from .service import FastGRPCService
 
 
 
+
 class FastGRPC:
     def __init__(self, *services: FastGRPCService, port: int = 50051, reflection: bool = False):
         self._server = server()
